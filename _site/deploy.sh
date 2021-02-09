e247423d53b33e702062fbccf83bcbbee03ea6d6
Theory Force <3662943-theoryforce@users.noreply.gitlab.com>
2021-02-09 09:01:50 +0100

    bak

--- _site/deploy.sh
+++ _site/deploy.sh
@@ -1,12 +1,18 @@
-mv -f _site/ _prev/
-bundle exec jekyll b
-diff -qr _site/ _prev/
-read -p "Deploy? [y/n]" yn
-case $yn in
-        [Yy]* ) git add . --all && git commit -m "autopublish rpc" && git push origin master;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-esac
-rm -rf _prev/
-echo "done."
+#!/bin/sh
+# Commit mesage
 
+if [ -z "$1" ]; then
+ echo "Submit a commit message you uncultured swine!"
+ exit
+fi
+echo " === [1] build"
+BUNDLE_GEMFILE='./Gemfile' bundle exec jekyll b --source ./ --destination ./_site
+
+echo " === [2] push"
+git add .
+git commit -m "$1"
+git push origin master
+
+#exit
+echo " === [3] publish"
+scp -r ./_site/* sgp:/var/www/atnet/
